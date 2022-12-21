e54ab23ac02cd73fa43aeb1baa8b7120fb105b66
elonehoo <elonehoo@gmail.com>
2022-12-21 12:51:09 +0800

    empty

--- .vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import './style/index.css'
-import 'uno.css'
-import '@unocss/reset/tailwind.css'
-
-import Layout from './Layout.vue'
-
-export default {
-  Layout,
-}
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import Unocss from 'unocss/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Unocss()
-  ],
-})
