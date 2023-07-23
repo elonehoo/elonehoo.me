c25daac57aa8fdfa395d3c7b195264a2092ac8a5
elonehoo <hi@elonehoo.me>
2023-07-23 20:59:27 +0800

    fix: update head og

--- app.vue
+++ app.vue
@@ -4,12 +4,15 @@ useHead({
     { property: 'og:title', content: 'Elone Hoo' },
     { property: 'og:image', content: '/og.png' },
     { property: 'og:description', content: 'Elone Hoo\'s Portfolio' },
+    { property: 'og:url', content: 'https://elonehoo.me' },
     { name: 'description', content: 'Elone Hoo\'s Portfolio' },
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:creator', content: '@elonehoo' },
     { name: 'twitter:title', content: 'Elone Hoo' },
     { name: 'twitter:description', content: 'Elone Hoo\'s Portfolio' },
-    { name: 'twitter:image', content: '/og.png' }
+    { name: 'twitter:image', content: '/og.png' },
+    { name: 'title', content: 'Elone Hoo' },
+    { name: 'lang', content:'en' },
   ],
   link: [
     {
