49f290ea8d1c5d4bf106056cf035005361fc6c26
elonehoo <hi@elonehoo.me>
2023-08-21 22:31:28 +0800

    style: run eslint fix

--- layouts/Demos.vue
+++ layouts/Demos.vue
@@ -17,7 +17,7 @@ const demos = useContent().navigation.value?.filter((i: any) => i._path.startsWi
 
 const parts = computed(() => {
   const result = Array.from({ length: cols.value }, () => [] as typeof demos)
-  demos.forEach((item:any, i:any) => {
+  demos.forEach((item: any, i: any) => {
     result[i % cols.value].push(item)
   })
   return result
@@ -55,28 +55,29 @@ useHead({
             :key="demo.link"
             :href="demo.link"
             class="[background:#ffffff] dark:[background:#000000] p-4px cursor-pointer block w-full [list-style:none] h-fit relative border overflow-hidden mb-1 rounded-xl border-solid border-#e8e8e8 hover:border-b-[#e8e8e8]! dark:border-[#2e2e2e] dark:hover:border-b-[#2e2e2e]! [transform:translateZ(0px)] text-inherit! no-underline!"
-            >
-              <div
-                class="
+          >
+            <div
+              class="
                   relative overflow-hidden rounded-lg [transform:translateZ(0px)]
                   [&::after]:content-[''] [&::after]:absolute [&::after]:w-full [&::after]:h-[200px] [&::after]:[transition:opacity_200ms_ease_0s] [&::after]:-bottom-16
                   bg-after
-              ">
-                <video v-if="demo.type === 'video'" playsinline loop autoplay class="relative block w-full h-full object-cover box-border m-0 my-0! object-contain overflow-clip [overflow-clip-margin:content-box]" :src="`/demos/${demo.date}.mp4`"></video>
-                <img
-                  v-else
-                  class="text-transparent aspect-[1.90476_/_1] block w-full h-full object-cover box-border m-0 my-0!"
-                  :src="`/demos/${demo.date}.png`"
-                />
-                <div class="w-full h-8 gap-3 absolute z-[1] [transition:opacity_200ms_ease_0s] whitespace-nowrap p-4 left-0 bottom-2 flex-nowrap justify-between items-center flex-row box-border flex">
-                  <p class="text-[rgb(237,237,237)] gap-1 overflow-hidden whitespace-nowrap text-ellipsis font-normal leading-7 text-sm block m-0">{{ demo.desc }}</p>
-                  <p class="text-[rgba(255,255,255,0.592)] font-normal leading-7 text-sm block m-0">{{ dayjs(demo.date).format('MMM D, YYYY') }}</p>
-                </div>
+              "
+            >
+              <video v-if="demo.type === 'video'" playsinline loop autoplay class="relative block w-full h-full object-cover box-border m-0 my-0! object-contain overflow-clip [overflow-clip-margin:content-box]" :src="`/demos/${demo.date}.mp4`" />
+              <img
+                v-else
+                class="text-transparent aspect-[1.90476_/_1] block w-full h-full object-cover box-border m-0 my-0!"
+                :src="`/demos/${demo.date}.png`"
+              >
+              <div class="w-full h-8 gap-3 absolute z-[1] [transition:opacity_200ms_ease_0s] whitespace-nowrap p-4 left-0 bottom-2 flex-nowrap justify-between items-center flex-row box-border flex">
+                <p class="text-[rgb(237,237,237)] gap-1 overflow-hidden whitespace-nowrap text-ellipsis font-normal leading-7 text-sm block m-0">{{ demo.desc }}</p>
+                <p class="text-[rgba(255,255,255,0.592)] font-normal leading-7 text-sm block m-0">{{ dayjs(demo.date).format('MMM D, YYYY') }}</p>
               </div>
-              <div class="h-10 items-center justify-center text-sm font-medium text-#171717 dark:text-[#ededed] flex gap-1 mt-1 rounded-lg [background:#f3f3f3] hover:[background:#ededed] dark:[background:#232323] hover:[background:#282828] [transition:background_150ms_ease_0s,_color_150ms_ease_0s]">
-                {{ demo.meta }} <div i-ri-arrow-right-line/>
-              </div>
-            </a>
+            </div>
+            <div class="h-10 items-center justify-center text-sm font-medium text-#171717 dark:text-[#ededed] flex gap-1 mt-1 rounded-lg [background:#f3f3f3] hover:[background:#ededed] dark:[background:#232323] hover:[background:#282828] [transition:background_150ms_ease_0s,_color_150ms_ease_0s]">
+              {{ demo.meta }} <div i-ri-arrow-right-line />
+            </div>
+          </a>
         </div>
       </div>
     </main>
