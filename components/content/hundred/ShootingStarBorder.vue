7b0d952c6ba2511bc2bca598e232391af356ca18
elonehoo <hi@elonehoo.me>
2023-12-03 13:32:22 +0800

    fix: add demo light mode

--- components/content/hundred/ShootingStarBorder.vue
+++ components/content/hundred/ShootingStarBorder.vue
@@ -4,20 +4,24 @@
 
 <template>
   <CraftBox>
-    <button class="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
+    <button class="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_80%)_inset] dark:shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
       <span>
-        <span class="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+        <span class="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] animate-rotate light-pursuit before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
       </span>
-      <span class="backdrop absolute inset-[1px] rounded-full bg-neutral-950 transition-colors duration-200 group-hover:bg-neutral-800" />
-      <span class="z-10 py-0.5 text-sm text-neutral-100">Get notified</span>
+      <span class="backdrop absolute inset-[1px] rounded-full bg-gray-100 dark:bg-neutral-950 transition-colors duration-200 group-hover:bg-light-950 dark:group-hover:bg-neutral-800" />
+      <span class="z-10 py-0.5 text-sm text-dark dark:text-neutral-100">Get notified</span>
     </button>
   </CraftBox>
 </template>
 
 <style scoped>
-.before\:bg-\[conic-gradient\(from_0deg\2c transparent_0_340deg\2c white_360deg\)\]:before {
+.dark .light-pursuit:before {
     content: "";
     background-image: conic-gradient(from 0deg,transparent 0 340deg,#fff 1turn);
+}
+.light-pursuit:before {
+    content: "";
+    background-image: conic-gradient(from 0deg,transparent 0 340deg,#000 1turn);
 }
 .animate-rotate:before {
     content: "";
