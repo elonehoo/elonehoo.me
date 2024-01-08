29221fd5aea299917e2bcc98efd008173d1b5d7b
elonehoo <hi@elonehoo.me>
2024-01-08 18:13:41 +0800

    fix: removed the long image, leaving only the horizontal shot, so that the animation is correct

--- layouts/gallery.vue
+++ layouts/gallery.vue
@@ -31,11 +31,11 @@ useHead({
   <DefaultLayout>
     <main class="p-0 md:p-2 flex flex-col relative min-h-screen overflow-hidden mx-0 md:mx-auto my-0 box-border">
       <div class="grid grid-cols-[repeat(auto-fit,minmax(440px,1fr))] gap-2 box-border">
-        <div v-for="photo, idx in gallery" :key="photo.url" class="inline-block relative h-[700px] [outline:0px] slide-enter" :style="`--enter-stage: ${idx};`" style="content: none;">
+        <div v-for="photo, idx in gallery" :key="photo.url" class="inline-block relative h-[350px] [outline:0px] slide-enter" :style="`--enter-stage: ${idx};`" style="content: none;">
           <PreviewRoot class="transition-transform ease-in-out duration-500 outline-none">
             <template #default>
               <img
-                class="absolute h-700px w-full text-transparent inset-0 select-none object-cover my-0! rounded-2xl"
+                class="absolute h-350px w-full text-transparent inset-0 select-none object-cover my-0! rounded-2xl"
                 :src="`/gallery${photo.url}`"
                 :alt="photo.url"
               >
