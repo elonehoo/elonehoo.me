0fdd9a879fefff8e84ccf3a10bba901c439f353f
elonehoo <hi@elonehoo.me>
2024-10-16 22:44:10 +0800

    wip

--- content/.vitepress/components/AboutPage.vue
+++ content/.vitepress/components/AboutPage.vue
@@ -4,13 +4,13 @@
 
 <template>
   <div class="mt-[--h-margin] pt-[--h-margin]">
-    <section class="relative">
-      <div>
-        <h1 class="text-[calc(.3vmax_+_.3vmin_+_1.5rem)] font-bold text-foreground leading-1.26">
-          <span class="after:content-['~'] after:mx-2 after:text-main">elonehoo.me</span>
-          <span>关于我</span>
-        </h1>
-        <slot />
+    <section class="">
+      <h1 class="text-[calc(.3vmax_+_.3vmin_+_1.5rem)] font-bold text-foreground leading-1.26">
+        <span class="after:content-['~'] after:mx-2 after:text-main">elonehoo.me</span>
+        <span>关于我</span>
+      </h1>
+      <div class="w-full flex flex-wrap border-t border-t-dotted border-t-border mt-[calc(var(--h-margin)_*_2)]">
+        <!--  -->
       </div>
     </section>
   </div>
