f63831a8ef7d9cdb7a2b20d3168e9099ce7d183b
elonehoo <hi@elonehoo.me>
2024-11-29 13:43:20 +0800

    wip

---
+++ theme/components/LinkButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Link } from 'destyler'
+import { Icones } from '~/index'
+
+const props = defineProps<{
+  link: string
+}>()
+</script>
+
+<template>
+  <Link
+    :to="props.link"
+    target="_self"
+    class="
+    flex items-center gap-x-0.5 w-fit text-gray-11 text-sm
+    hover:bg-action hover:text-gray-12 px-1.5 py-1 rounded-sm
+    font-medium border border-gray-12 hover:border-action
+    cursor-pointer after:bg-transparent!"
+  >
+    <slot />
+    <Icones name="Right" class="w-3 h-3" />
+  </Link>
+</template>
---
+++ theme/pages/List.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useConfig } from '~/vitepress/composables/config'
+
+const { config } = useConfig()
+</script>
+
+<template>
+  <div v-for="nav in config.nav" :key="nav.text" class="flex gap-x-3 mt-6 items-center">
+    <LinkButton
+      v-for="item in nav.items"
+      :key="item.link"
+      :link="item.link"
+    >
+      {{ item.text }}
+    </LinkButton>
+  </div>
+</template>
