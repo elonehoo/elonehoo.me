37843ff7cfe56bfeec3651f930ea860d98d0d2be
elonehoo <hi@elonehoo.me>
2024-12-12 13:23:07 +0800

    wip

--- theme/components/LinkButton.vue
+++ theme/components/LinkButton.vue
@@ -12,9 +12,9 @@ const props = defineProps<{
     :to="props.link"
     target="_self"
     class="
-    flex items-center gap-x-0.5 w-fit text-gray-11 text-sm
-    hover:bg-action hover:text-gray-12 px-1.5 py-1 rounded-sm
-    font-medium border border-gray-12 hover:border-action
+    flex items-center gap-x-0.5 w-fit text-gray-8 dark:text-gray-11 text-sm
+    hover:bg-action hover:text-gray-1! dark:hover:text-gray-12! px-1.5 py-1 rounded-sm
+    font-medium border border-gray-6 dark:border-gray-12 hover:border-action
     cursor-pointer after:bg-transparent!"
   >
     <slot />
