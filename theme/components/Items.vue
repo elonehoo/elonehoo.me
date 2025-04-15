79f6a3310ab988faf161a00e5442e77aee46be7b
elonehoo <hi@elonehoo.me>
2025-04-15 22:01:03 +0800

    feat: add new post

--- theme/components/Items.vue
+++ theme/components/Items.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 
 <template>
   <div class="flex flex-col mt-4 group">
-    <div class="flex justify-between md:gap-x-16 gap-x-4">
+    <div class="flex justify-between md:gap-x-8 gap-x-4">
       <Link :to="props.record.url" target="_self">
         {{ props.record.title }}
       </Link>
