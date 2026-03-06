b7c8e7f6818d9802859cd40eac274d6102e3c52a
elonehoo <hi@elonehoo.me>
2026-03-06 12:43:33 +0800

    feat: update pnpm-lock.yaml with new dependencies and versions; add skills-lock.json for skill management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   modules: [
-    '@nuxt/content', 
-    '@unocss/nuxt', 
-    '@nuxt/eslint'
+    '@nuxt/content',
+    '@unocss/nuxt',
+    '@nuxt/eslint',
+    'nuxt-studio'
   ],
   devtools: { 
     enabled: true 
@@ -15,6 +18,16 @@ export default defineNuxtConfig({
   eslint: {
     config: {
       standalone: false
+    }
+  },
+  content: {
+    build: {
+      markdown: {
+        // Object syntax can be used to override default options
+        remarkPlugins: {
+          'remark-supersub': {},
+        },
+      }
     }
   }
 })
