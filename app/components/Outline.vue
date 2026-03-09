e5189e8b800a1f7459d21430b5b2f007ad66adfe
elonehoo <hi@elonehoo.me>
2026-03-09 14:05:33 +0800

    wip

--- app/components/Outline.vue
+++ app/components/Outline.vue
@@ -3,12 +3,14 @@ const { config } = useAppConfig()
 </script>
 
 <template>
-  <AsideItem
-    v-for="social in config.socialLinks"
-    :key="social.link"
-    :link="social.link"
-    :icon="social.icon"
-  >
-    {{ social.icon }}
-  </AsideItem>
+  <div class="op-0 lg:op-100 lg:fixed lg:top-12 lg:right-12">
+    <AsideItem
+      v-for="social in config.socialLinks"
+      :key="social.link"
+      :link="social.link"
+      :icon="social.icon"
+    >
+      {{ social.icon }}
+    </AsideItem>
+  </div>
 </template>
